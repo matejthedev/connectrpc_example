be4b18eabece57819885a41d62074a4bcadc1334
matejpavic <mpavic.dev@gmail.com>
2024-09-28 23:52:13 +0200

    Initial commit of simple server

---
+++ cmd/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"connectrpc.com/connect"
+	greetv1 "connectrpc_example/gen/greet/v1"
+	"connectrpc_example/gen/greet/v1/greetv1connect"
+	"context"
+	"fmt"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"log"
+	"net/http"
+)
+
+type GreetServer struct{}
+
+func (*GreetServer) Greet(ctx context.Context, req *connect.Request[greetv1.GreetRequest]) (*connect.Response[greetv1.GreetResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&greetv1.GreetResponse{Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name)})
+	return res, nil
+}
+
+func main() {
+	greeter := &GreetServer{}
+	mux := http.NewServeMux()
+	path, handler := greetv1connect.NewGreetServiceHandler(greeter)
+	mux.Handle(path, handler)
+	err := http.ListenAndServe(
+		"localhost:8888",
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+	if err != nil {
+		log.Fatal("failed to start a server")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module connectrpc_example
+
+go 1.21
+
+toolchain go1.23.0
+
+require (
+	connectrpc.com/connect v1.17.0
+	golang.org/x/net v0.23.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require golang.org/x/text v0.14.0 // indirect
